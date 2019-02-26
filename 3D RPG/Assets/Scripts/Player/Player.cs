ec9183a230ae961b0e41fcbcd59f79a975b28baf
Tsantas Haris <ctsantas@arx.gr>
2019-02-26 17:23:38 +0200

    added player script

---
+++ 3D RPG/Assets/Scripts/Player/Player.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] float maxHealthPoints = 100f;
+
+    float currentHealthPoints = 100f;
+
+    public float HealthAsPercentage {
+        get {
+            return currentHealthPoints / maxHealthPoints;
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
